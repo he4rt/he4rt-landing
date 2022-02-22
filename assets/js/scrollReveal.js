c5ec9de10736703e73ce08548077c804a0096d01
thel3onardo <leonardo.contato03@gmail.com>
2022-02-22 15:21:36 -0300

    feat: new style classes created

---
+++ assets/js/scrollReveal.js
@@ -0,0 +1,2 @@
+ScrollReveal().reveal('.text-animation', { distance: '30px', duration: 1200, delay: 600 });
+ScrollReveal().reveal('.card-animation', { distance: '40px', duration: 1000, delay: 900 })
