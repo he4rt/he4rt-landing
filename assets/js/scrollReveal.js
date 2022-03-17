93d43948e4383347f5b088d4c91ed07824fb5b45
leonardorafaeldev <leonardorafaeldev@gmail.com>
2022-03-17 20:29:44 -0300

    🐛 color at the top edge leaking

--- assets/js/scrollReveal.js
+++ assets/js/scrollReveal.js
@@ -1,4 +1,3 @@
-window.sr = ScrollReveal({ reset:true });
 
 ScrollReveal().reveal('.text-animation', { distance: '30px', duration: 1200, delay: 600 });
 ScrollReveal().reveal('.card-animation', { distance: '40px', duration: 1000, delay: 900 });
