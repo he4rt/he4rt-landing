d480405c436b3f7dc19c28a9a09d45fe561177ce
leonardorafaeldev <leonardorafaeldev@gmail.com>
2022-03-17 13:56:58 -0300

    add animation test whit scrollreveal

--- assets/js/scrollReveal.js
+++ assets/js/scrollReveal.js
@@ -1,3 +1,12 @@
+window.sr = ScrollReveal({ reset:true });
+
 ScrollReveal().reveal('.text-animation', { distance: '30px', duration: 1200, delay: 600 });
 ScrollReveal().reveal('.card-animation', { distance: '40px', duration: 1000, delay: 900 });
 ScrollReveal().reveal('#header');
+ScrollReveal().reveal('.scroll-left', {origin: 'left', distance: '40px', duration: 1000, delay: 500 });
+ScrollReveal().reveal('.scroll-left-2', {origin: 'left', distance: '40px', duration: 1000, delay: 800 });
+ScrollReveal().reveal('.scroll-left-3', {origin: 'left', distance: '40px', duration: 1000, delay: 1200 });
+ScrollReveal().reveal('.scroll-right', {origin: 'right', distance: '40px', duration: 1000, delay: 500 });
+ScrollReveal().reveal('.scroll-bottom', {origin: 'bottom', distance: '40px', duration: 1000, delay: 500 });
+ScrollReveal().reveal('.scroll-bottom-2', {origin: 'bottom', distance: '40px', duration: 1000, delay: 800 });
+ScrollReveal().reveal('.scroll-bottom-3', {origin: 'bottom', distance: '40px', duration: 1000, delay: 1500 });
