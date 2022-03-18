6aa46ca4af1156d5c29cfc24adfb8fc080f315f9
thel3onardo <leonardo.contato03@gmail.com>
2022-03-17 21:30:28 -0300

    refactor: More improvements in swiper

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,19 +1,17 @@
-import '../css/style.css';
-
 const swiperContainer = document.querySelector('.swiper-wrapper');
 const forNoobs = [
-    { img_url: 'https://yt3.ggpht.com/g8lfkmyHXwpjIBHoNgHgip2m4LSnR-8gsVa-1DG05JGVuTrIIjJ9x4tUNuYvDuH7OHyCHqnnwQ=s900-c-k-c0x00ffffff-no-rj', owner: 'Mc Poze do Rodo', role: 'Músico', name: 'Nice', category: 'Funk', content: 'Lorem ipsum meu deus do ceu mata o khazix pelo menos yoda', url: 'https://github.com/' },
-    { img_url: 'https://yt3.ggpht.com/g8lfkmyHXwpjIBHoNgHgip2m4LSnR-8gsVa-1DG05JGVuTrIIjJ9x4tUNuYvDuH7OHyCHqnnwQ=s900-c-k-c0x00ffffff-no-rj', owner: 'Mc Poze do Rodo', role: 'Músico', name: 'Nice', category: 'Funk', content: 'Lorem ipsum meu deus do ceu mata o khazix pelo menos yodaqwdwjiqndjiqwndjqwndwiqndiqundwquindiquwdnuiqndiqhdhuqwdhuiqdhuiqhu', url: 'https://github.com/' },
-    { img_url: 'https://yt3.ggpht.com/g8lfkmyHXwpjIBHoNgHgip2m4LSnR-8gsVa-1DG05JGVuTrIIjJ9x4tUNuYvDuH7OHyCHqnnwQ=s900-c-k-c0x00ffffff-no-rj', owner: 'Mc Poze do Rodo', role: 'Músico', name: 'Nice', category: 'Funk', content: 'Lorem ipsum meu deus do ceu mata o khazix pelo menos yoda', url: 'https://github.com/' }
+    { img_url: 'https://yt3.ggpht.com/g8lfkmyHXwpjIBHoNgHgip2m4LSnR-8gsVa-1DG05JGVuTrIIjJ9x4tUNuYvDuH7OHyCHqnnwQ=s900-c-k-c0x00ffffff-no-rj', owner: 'Mc Poze do Rodo', role: 'Músico', name: 'Nice', category: 'Funk', content: 'Lorem ipsum meu deus do ceu mata o khazix pelo menos yoda', url: 'https://github.com/', alt: 'Alt do componente' },
+    { img_url: 'https://yt3.ggpht.com/g8lfkmyHXwpjIBHoNgHgip2m4LSnR-8gsVa-1DG05JGVuTrIIjJ9x4tUNuYvDuH7OHyCHqnnwQ=s900-c-k-c0x00ffffff-no-rj', owner: 'Mc Poze do Rodo', role: 'Músico', name: 'Nice', category: 'Funk', content: 'Lorem ipsum meu deus do ceu mata o khazix pelo menos yodaqwdwjiqndjiqwndjqwndwiqndiqundwquindiquwdnuiqndiqhdhuqwdhuiqdhuiqhu', url: 'https://github.com/', alt: 'Alt do componente' },
+    { img_url: 'https://yt3.ggpht.com/g8lfkmyHXwpjIBHoNgHgip2m4LSnR-8gsVa-1DG05JGVuTrIIjJ9x4tUNuYvDuH7OHyCHqnnwQ=s900-c-k-c0x00ffffff-no-rj', owner: 'Mc Poze do Rodo', role: 'Músico', name: 'Nice', category: 'Funk', content: 'Lorem ipsum meu deus do ceu mata o khazix pelo menos yoda', url: 'https://github.com/', alt: 'Alt do componente' }
 ]
 forNoobs.forEach((forNoob) => {
     swiperContainer.insertAdjacentHTML('beforeend', `
     <div class="swiper-slide">
         <div class="flex flex-col rounded-lg w-44 md:w-72 drop-shadow-xl overflow-hidden">
-            <div class="w-full bg-gradient-to-r from-purple-500 to-pink flex items-center justify-between text-white p-3">
+            <div class="w-full bg-gradient-to-r from-purple-500 to-pink-700 flex items-center justify-between text-white p-4">
                 <div class="flex items-center">
-                    <div class="w-1/4 p-2 mr-3">
-                        <img src="https://yt3.ggpht.com/g8lfkmyHXwpjIBHoNgHgip2m4LSnR-8gsVa-1DG05JGVuTrIIjJ9x4tUNuYvDuH7OHyCHqnnwQ=s900-c-k-c0x00ffffff-no-rj" class="rounded-full shadow-2xl">
+                    <div class="w-1/4 mw-35px mh-35px rounded-full overflow-hidden mr-4">
+                        <img src="https://images2.fanpop.com/image/photos/11400000/Anime-girls-anime-stories-11411029-425-601.jpg" class="shadow-2xl drop-shadow" alt="${forNoob.alt}">
                     </div>
                     <div>
                         <h2 class="text-white font-bold font-spline">Mc Poze do Rodo</h2>
