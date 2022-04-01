b05d6be53d3d469204dd0edfb3476a8d4bd6f568
thel3onardo <leonardo.contato03@gmail.com>
2022-03-31 23:03:16 -0300

    fix: Decreases header elements reveal time

--- assets/js/scrollReveal.js
+++ assets/js/scrollReveal.js
@@ -1,3 +1,8 @@
+ScrollReveal().reveal('.scroll-header-left-1', {origin: 'left', distance: '40px', duration: 1000, delay: 0});
+ScrollReveal().reveal('.scroll-header-left-2', {origin: 'left', distance: '40px', duration: 1000, delay: 150});
+ScrollReveal().reveal('.scroll-header-left-3', {origin: 'left', distance: '40px', duration: 1000, delay: 300});
+ScrollReveal().reveal('.scroll-header-right-1', {origin: 'right', distance: '40px', duration: 1000, delay: 150});
+
 ScrollReveal().reveal('.scroll-left-1', {origin: 'left', distance: '40px', duration: 1000, delay: 500 });
 ScrollReveal().reveal('.scroll-left-2', {origin: 'left', distance: '40px', duration: 1000, delay: 800 });
 ScrollReveal().reveal('.scroll-left-3', {origin: 'left', distance: '40px', duration: 1000, delay: 1000 });
