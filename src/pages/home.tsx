7279940b70938948f0f297df928dbb14de6dad13
Lucas Rosa <lucassantosrosa51@gmail.com>
2025-04-19 09:40:17 -0300

    feat: add Testimonial component

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,7 +1,37 @@
+import { Footer } from "@/components/Footer";
+import { Header } from "../components/Header";
+import { Articles } from "@/components/Articles";
+import { Testimonial } from "@/components/Testimonials";
+
 export const Home = () => {
   return (
-    <div>
-      <h1 className="text-red-500">Home</h1>
+    <div className="flex flex-col w-screen h-screen">
+      <Header />
+
+      <main className="flex-1 bg-primary ">
+        <div className="flex flex-col gap-4 w-full items-center">
+          <Testimonial
+            author={{
+              name: "Lucas Rosa",
+              role: "Frontend Developer",
+              imageSrc: "https://avatars.githubusercontent.com/u/55213609?v=4",
+            }}
+            description={
+              '"A comunidade Coração Dev mudou completamente minha trajetória profissional. Através dos projetos e mentorias, consegui meu primeiro emprego como desenvolvedor e hoje faço parte de uma empresa incrível."'
+            }
+          />
+          <Testimonial
+            isLoading={true}
+            author={{
+              name: "Lucas Rosa",
+              role: "Frontend Developer",
+              imageSrc: "https://avatars.githubusercontent.com/u/55213609?v=4",
+            }}
+          />
+        </div>
+      </main>
+
+      <Footer />
     </div>
   );
 };
