7fcf047b6e92e99a28ab0a45cca4bb57fcbe9097
Gabriel do Carmo Vieira <48625433+gvieira18@users.noreply.github.com>
2025-10-22 23:46:33 -0300

    chore: update .gitignore and tsconfig.json for static build

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,9 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: 'export',
+  skipTrailingSlashRedirect: true,
+  distDir: 'dist',
 };
 
 export default nextConfig;
